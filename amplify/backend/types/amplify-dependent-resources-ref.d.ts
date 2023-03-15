0b94743cd3196e3d0d011b55326ffe67577c10b5
your_username <pad128g@icloud.com>
2023-03-14 23:56:38 -0400

    update aws-export

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "hirebeatjobseeker3a26fbef": {
+    "HireBeatJobSeeker": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
