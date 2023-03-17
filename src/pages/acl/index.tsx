115fbe4ebb542923d71c51ce99ab92023bc38afa
your_username <pad128g@icloud.com>
2023-03-16 23:10:54 -0400

    Change ability

--- src/pages/acl/index.tsx
+++ src/pages/acl/index.tsx
@@ -26,7 +26,7 @@ const ACLPage = () => {
           </CardContent>
         </Card>
       </Grid>
-      {ability?.can('read', 'analytics') ? (
+      {ability?.can('read', 'acl-page') ? (
         <Grid item md={6} xs={12}>
           <Card>
             <CardHeader title='Analytics' />
