60afe675b6fd2f0749a8a8e40ac61aac33e21d36
your_username <pad128g@icloud.com>
2023-03-24 20:53:57 -0400

    update backend settings

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,11 +7,26 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_ndFPxkTt9",
     "aws_user_pools_web_client_id": "30ppesp9kkckpgq19fo50i54f6",
-    "oauth": {},
+    "oauth": {
+        "domain": "m9vaaf8flez5-staging.auth.us-east-1.amazoncognito.com",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "http://localhost:3000/profile",
+        "redirectSignOut": "http://localhost:3000/profile",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS",
     "aws_cognito_username_attributes": [
         "EMAIL"
     ],
-    "aws_cognito_social_providers": [],
+    "aws_cognito_social_providers": [
+        "GOOGLE"
+    ],
     "aws_cognito_signup_attributes": [
         "EMAIL"
     ],
