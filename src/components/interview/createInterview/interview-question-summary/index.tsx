d9ee2b83451404fe89ea35e568f158358deb9d70
your_username <pad128g@icloud.com>
2023-04-30 16:16:34 -0400

    rm unused vars

--- src/components/interview/createInterview/interview-question-summary/index.tsx
+++ src/components/interview/createInterview/interview-question-summary/index.tsx
@@ -1,6 +1,5 @@
 import { Button, Card, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import { auto } from '@popperjs/core'
 import React, { useState, useEffect } from 'react'
 
 interface InterviewQuestion {
