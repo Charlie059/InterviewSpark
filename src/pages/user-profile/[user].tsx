3bd2b00c6f1075da0709019805806bd6dc4862cd
shenfan0613 <shen.1450@osu.edu>
2023-05-17 00:39:22 -0400

    #TODO in [user].tsx

--- src/pages/user-profile/[user].tsx
+++ src/pages/user-profile/[user].tsx
@@ -53,7 +53,7 @@ export const getServerSideProps: GetServerSideProps = async ({params}: GetServer
     "fName": "Fan",
     "lName": "Shen",
     "photoImgURL": "https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg",
-    "coverImgURL": null,
+    "coverImgURL": 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9ef71780-bbed-494b-8633-05301786f9ce/dew3k8d-2bd14f7a-3d72-4757-881b-a19686520b38.png/v1/fill/w_1280,h_720/trans_flag_background_by_imoogi_nascent_dew3k8d-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvOWVmNzE3ODAtYmJlZC00OTRiLTg2MzMtMDUzMDE3ODZmOWNlXC9kZXczazhkLTJiZDE0ZjdhLTNkNzItNDc1Ny04ODFiLWExOTY4NjUyMGIzOC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.4Asaj6-IcvYga7ZMOc0fHf-Ru18jxTUT0yxIldfgubA',
     "resumeKey": "test.url",
     "addressLine1": "",
     "addressLine2": "",
@@ -61,7 +61,7 @@ export const getServerSideProps: GetServerSideProps = async ({params}: GetServer
     "state": "NY",
     "postalCode": "",
     "country": "USA",
-    "joiningDate": null,
+    "joiningDate": '2022-05-22',
     "contact": null
   }
 
