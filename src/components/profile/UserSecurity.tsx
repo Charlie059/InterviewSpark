de2705444b73341bbd6eebca6908ea3e450f694e
your_username <pad128g@icloud.com>
2023-07-19 17:14:13 -0400

    Update UserSecurity.tsx

--- src/components/profile/UserSecurity.tsx
+++ src/components/profile/UserSecurity.tsx
@@ -16,19 +16,19 @@ import OutlinedInput from '@mui/material/OutlinedInput'
 import InputAdornment from '@mui/material/InputAdornment'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import FormHelperText from "@mui/material/FormHelperText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import FormHelperText from '@mui/material/FormHelperText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
-import CheckIcon from '@mui/icons-material/Check';
+import CheckIcon from '@mui/icons-material/Check'
 
 // ** Third Party Imports
-import * as yup from 'yup';
+import * as yup from 'yup'
 
 interface State {
-  currentPassword:string
+  currentPassword: string
   showCurrentPassword: boolean
   newPassword: string
   showNewPassword: boolean
@@ -46,36 +46,40 @@ const UserSecurity = () => {
     confirmNewPassword: '',
     showConfirmNewPassword: false
   })
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState<Record<string, string>>({})
 
   // ** Validation Schema
   const schema = yup.object().shape({
-    currentPassword: yup.string().required("Current password is required"),
-    newPassword:
-      yup.string()
-        .notOneOf([yup.ref('currentPassword')], 'New password must be different from the current password')
-        .min(8, 'New password must be at least 8 characters')
-        .required("New password is required"),
-    confirmNewPassword: yup.string().oneOf([yup.ref('newPassword'), null], 'Passwords do not match').required("Confirm new password is required")
-  });
+    currentPassword: yup.string().required('Current password is required'),
+    newPassword: yup
+      .string()
+      .notOneOf([yup.ref('currentPassword')], 'New password must be different from the current password')
+      .min(8, 'New password must be at least 8 characters')
+      .required('New password is required'),
+    confirmNewPassword: yup
+      .string()
+      .oneOf([yup.ref('newPassword'), null], 'Passwords do not match')
+      .required('Confirm new password is required')
+  })
 
-// Define a function to handle form submission
+  // Define a function to handle form submission
   const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    schema.validate(values, { abortEarly: false })
+    schema
+      .validate(values, { abortEarly: false })
       .then(() => {
         // Form is valid
       })
-      .catch((validationErrors) => {
+      .catch(validationErrors => {
         // Form is invalid, set the errors state
-        const newErrors = {};
+        const newErrors: Record<string, string> = {}
         validationErrors.inner.forEach((error: { path: string | number; message: any }) => {
-          newErrors[error.path] = error.message;
-        });
-        setErrors(newErrors);
-      });
-  };
+          newErrors[error.path] = error.message
+        })
+        setErrors(newErrors)
+      })
+  }
 
   // Handle Current Password
   const handleCurrentPasswordChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
@@ -115,97 +119,99 @@ const UserSecurity = () => {
     <Grid container spacing={6}>
       <Grid item xs={12}>
         <Card>
-          <CardHeader title='Change Password' sx={{m:2}}/>
-          <CardContent sx={{ml:5, mr:5}}>
+          <CardHeader title='Change Password' sx={{ m: 2 }} />
+          <CardContent sx={{ ml: 5, mr: 5 }}>
             <Grid container spacing={10}>
               <Grid item xs={12} sm={12} md={7} lg={7}>
                 <form onSubmit={handleSubmit}>
-                <Grid container spacing={6}>
-                  <Grid item xs={12}>
-                    <FormControl fullWidth error={!!errors}>
-                      <InputLabel htmlFor='user-view-security-current-password'>Current Password</InputLabel>
-                      <OutlinedInput
-                        label='Current Password'
-                        value={values.currentPassword}
-                        id='user-view-security-current-password'
-                        onChange={handleCurrentPasswordChange('currentPassword')}
-                        type={values.showCurrentPassword ? 'text' : 'password'}
-                        endAdornment={
-                          <InputAdornment position='end'>
-                            <IconButton
-                              edge='end'
-                              onClick={handleClickShowCurrentPassword}
-                              aria-label='toggle password visibility'
-                              onMouseDown={handleMouseDownCurrentPassword}
-                            >
-                              <Icon icon={values.showCurrentPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                      />
-                      {errors.currentPassword && <FormHelperText>{errors.currentPassword}</FormHelperText>}
-                    </FormControl>
-                  </Grid>
+                  <Grid container spacing={6}>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth error={!!errors}>
+                        <InputLabel htmlFor='user-view-security-current-password'>Current Password</InputLabel>
+                        <OutlinedInput
+                          label='Current Password'
+                          value={values.currentPassword}
+                          id='user-view-security-current-password'
+                          onChange={handleCurrentPasswordChange('currentPassword')}
+                          type={values.showCurrentPassword ? 'text' : 'password'}
+                          endAdornment={
+                            <InputAdornment position='end'>
+                              <IconButton
+                                edge='end'
+                                onClick={handleClickShowCurrentPassword}
+                                aria-label='toggle password visibility'
+                                onMouseDown={handleMouseDownCurrentPassword}
+                              >
+                                <Icon icon={values.showCurrentPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                        {errors.currentPassword && <FormHelperText>{errors.currentPassword}</FormHelperText>}
+                      </FormControl>
+                    </Grid>
 
-                  <Grid item xs={12}>
-                    <FormControl fullWidth error={!!errors.newPassword}>
-                      <InputLabel htmlFor='user-view-security-new-password'>New Password</InputLabel>
-                      <OutlinedInput
-                        label='New Password'
-                        value={values.newPassword}
-                        id='user-view-security-new-password'
-                        onChange={handleNewPasswordChange('newPassword')}
-                        type={values.showNewPassword ? 'text' : 'password'}
-                        endAdornment={
-                          <InputAdornment position='end'>
-                            <IconButton
-                              edge='end'
-                              onClick={handleClickShowNewPassword}
-                              aria-label='toggle password visibility'
-                              onMouseDown={handleMouseDownNewPassword}
-                            >
-                              <Icon icon={values.showNewPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                      />
-                      {errors.newPassword && <FormHelperText>{errors.newPassword}</FormHelperText>}
-                    </FormControl>
-                  </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth error={!!errors.newPassword}>
+                        <InputLabel htmlFor='user-view-security-new-password'>New Password</InputLabel>
+                        <OutlinedInput
+                          label='New Password'
+                          value={values.newPassword}
+                          id='user-view-security-new-password'
+                          onChange={handleNewPasswordChange('newPassword')}
+                          type={values.showNewPassword ? 'text' : 'password'}
+                          endAdornment={
+                            <InputAdornment position='end'>
+                              <IconButton
+                                edge='end'
+                                onClick={handleClickShowNewPassword}
+                                aria-label='toggle password visibility'
+                                onMouseDown={handleMouseDownNewPassword}
+                              >
+                                <Icon icon={values.showNewPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                        {errors.newPassword && <FormHelperText>{errors.newPassword}</FormHelperText>}
+                      </FormControl>
+                    </Grid>
 
-                  <Grid item xs={12}>
-                    <FormControl fullWidth error={!!errors.confirmNewPassword}>
-                      <InputLabel htmlFor='user-view-security-confirm-new-password'>Confirm New Password</InputLabel>
-                      <OutlinedInput
-                        label='Confirm New Password'
-                        value={values.confirmNewPassword}
-                        id='user-view-security-confirm-new-password'
-                        type={values.showConfirmNewPassword ? 'text' : 'password'}
-                        onChange={handleConfirmNewPasswordChange('confirmNewPassword')}
-                        endAdornment={
-                          <InputAdornment position='end'>
-                            <IconButton
-                              edge='end'
-                              aria-label='toggle password visibility'
-                              onClick={handleClickShowConfirmNewPassword}
-                              onMouseDown={handleMouseDownConfirmNewPassword}
-                            >
-                              <Icon icon={values.showConfirmNewPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                      />
-                      {errors.confirmNewPassword && <FormHelperText>{errors.confirmNewPassword}</FormHelperText>}
-                    </FormControl>
-                  </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth error={!!errors.confirmNewPassword}>
+                        <InputLabel htmlFor='user-view-security-confirm-new-password'>Confirm New Password</InputLabel>
+                        <OutlinedInput
+                          label='Confirm New Password'
+                          value={values.confirmNewPassword}
+                          id='user-view-security-confirm-new-password'
+                          type={values.showConfirmNewPassword ? 'text' : 'password'}
+                          onChange={handleConfirmNewPasswordChange('confirmNewPassword')}
+                          endAdornment={
+                            <InputAdornment position='end'>
+                              <IconButton
+                                edge='end'
+                                aria-label='toggle password visibility'
+                                onClick={handleClickShowConfirmNewPassword}
+                                onMouseDown={handleMouseDownConfirmNewPassword}
+                              >
+                                <Icon
+                                  icon={values.showConfirmNewPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'}
+                                />
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                        {errors.confirmNewPassword && <FormHelperText>{errors.confirmNewPassword}</FormHelperText>}
+                      </FormControl>
+                    </Grid>
 
-                  <Grid item xs={12}>
-                    <Button type='submit' variant='contained' sx={{mb:[0, 0, 5, 5]}}>
-                      Update
-                    </Button>
+                    <Grid item xs={12}>
+                      <Button type='submit' variant='contained' sx={{ mb: [0, 0, 5, 5] }}>
+                        Update
+                      </Button>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </form>
+                </form>
               </Grid>
               <Grid item xs={12} sm={12} md={5} lg={5}>
                 <Alert icon={false} severity='warning' sx={{ mb: 6 }}>
@@ -217,16 +223,16 @@ const UserSecurity = () => {
                       <ListItemIcon>
                         <CheckIcon color='warning' fontSize='small'></CheckIcon>
                       </ListItemIcon>
-                      <ListItemText primary="Minimum 8 characters long" disableTypography/>
+                      <ListItemText primary='Minimum 8 characters long' disableTypography />
                     </ListItem>
                     <ListItem>
                       <ListItemIcon>
                         <CheckIcon color='warning' fontSize='small'></CheckIcon>
                       </ListItemIcon>
-                      <ListItemText primary="New password must be different from current password" disableTypography/>
+                      <ListItemText primary='New password must be different from current password' disableTypography />
                     </ListItem>
                   </List>
-              </Alert>
+                </Alert>
               </Grid>
             </Grid>
           </CardContent>
